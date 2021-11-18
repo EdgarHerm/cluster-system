191af81fe5d15f9f19274ca918d5679e04ab9182
DeadCousing <57259646+DeadCousing@users.noreply.github.com>
2021-11-17 20:35:21 -0600

    test u

--- __init__.py
+++ __init__.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore
 import os
 # Creamos una instancia de SQLAlchemy
+from .models import Usuario, Rol
 dbSQL = SQLAlchemy()
-from models import Usuario, Rol
+
 
 userDataStore = SQLAlchemyUserDatastore(dbSQL, Usuario, Rol)
 
