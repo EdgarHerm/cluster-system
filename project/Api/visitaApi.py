6b9bb6af6a9bba21b054a2a9b11698dc195d48db
DeadCousing <57259646+DeadCousing@users.noreply.github.com>
2021-11-29 12:48:35 -0600

    Cambio en la peticion de visitas

--- project/Api/visitaApi.py
+++ project/Api/visitaApi.py
@@ -18,11 +18,25 @@ def buscarVisita():
             visitas = consultarVisitas(0)
             if len(visitas)>0:
                 Visitas_json = []
-                for visita in visitas:
-                    visitas_dictionary = visita.__dict__
-                    del visitas_dictionary['_sa_instance_state']
-                    Visitas_json.append(visitas_dictionary)
-                return jsonify(Visitas_json)
+                for x in visitas:
+                    Visitas_json.append({
+                        "idVisita":x.Visita.idVisita,
+                        "nombreV":x.Visita.nombre,
+                        "matricula":x.Visita.matriculaVehiculo,
+                        "modelo":x.Visita.modelo,
+                        "color": x.Visita.color,
+                        "estatusV":x.Visita.estatus,
+                        "fechaEntrada":x.Visita.fechaEntrada,
+                        "fechaSalida":x.Visita.fechaSalida,
+                        "idColono":x.Colono.idColono,
+                        "nombre": x.Persona.nombre,
+                        "Apellidos":x.Persona.Apellidos,
+                        "idDomicilio":x.Domicilio.idDomicilio,
+                        "calle": x.Domicilio.calle,
+                        "numero": x.Domicilio.numero,
+                        "descripcion":x.Domicilio.descripcion
+                    })
+                return jsonify({"visitas":Visitas_json})
         else:
             visita = consultarVisitas(request.json["idVisita"])
             if visita is None:
@@ -30,9 +44,96 @@ def buscarVisita():
                         "estado" : "ADVERTENCIA",
                         "mensaje": "No se encontro un visita con el id especificado"
                     })
-            visitas_dictionary = visita.__dict__
-            del visitas_dictionary['_sa_instance_state']
-            return jsonify(visitas_dictionary)
+            Visitas_json = []
+            for x in visita:
+                Visitas_json.append({
+                    "idVisita":x.Visita.idVisita,
+                    "nombreV":x.Visita.nombre,
+                    "matricula":x.Visita.matriculaVehiculo,
+                    "modelo":x.Visita.modelo,
+                    "color": x.Visita.color,
+                    "estatusV":x.Visita.estatus,
+                    "fechaEntrada":x.Visita.fechaEntrada,
+                    "fechaSalida":x.Visita.fechaSalida,
+                    "idColono":x.Colono.idColono,
+                    "nombre": x.Persona.nombre,
+                    "Apellidos":x.Persona.Apellidos,
+                    "idDomicilio":x.Domicilio.idDomicilio,
+                    "calle": x.Domicilio.calle,
+                    "numero": x.Domicilio.numero,
+                    "descripcion":x.Domicilio.descripcion
+                })
+            return jsonify({"visitas":Visitas_json})
+        
+    except Exception as e:
+        estado = "ERROR"
+        mensaje = "Ha ocurrido un error! Por favor verificalo con un administrador"
+        return jsonify({
+            "estado"  : estado,
+            "mensaje" : mensaje,
+            "excepcion": str(e)
+        })
+
+@visitaApi.route("/mostrarVisitaE", methods=['POST'])
+def buscarVisita():
+    estado = "OK"
+    mensaje = "Información consultada correctamente"
+    
+    try:
+        print(request.json)
+        print("idColono" not in request.json)
+        
+        if "idColono" not in request.json or request.json["idColono"] == 0:
+            print("test")
+            visitas = consultarVisitasEmpleado(0)
+            if len(visitas)>0:
+                Visitas_json = []
+                for x in visitas:
+                    Visitas_json.append({
+                        "idVisita":x.Visita.idVisita,
+                        "nombreV":x.Visita.nombre,
+                        "matricula":x.Visita.matriculaVehiculo,
+                        "modelo":x.Visita.modelo,
+                        "color": x.Visita.color,
+                        "estatusV":x.Visita.estatus,
+                        "fechaEntrada":x.Visita.fechaEntrada,
+                        "fechaSalida":x.Visita.fechaSalida,
+                        "idColono":x.Colono.idColono,
+                        "nombre": x.Persona.nombre,
+                        "Apellidos":x.Persona.Apellidos,
+                        "idDomicilio":x.Domicilio.idDomicilio,
+                        "calle": x.Domicilio.calle,
+                        "numero": x.Domicilio.numero,
+                        "descripcion":x.Domicilio.descripcion
+                    })
+                return jsonify({"visitas":Visitas_json})
+        else:
+            visita = consultarVisitasEmpleado(request.json["idColono"])
+            if visita is None:
+                    return jsonify({
+                        "estado" : "ADVERTENCIA",
+                        "mensaje": "No se encontro un visita con el id especificado"
+                    })
+            Visitas_json = []
+            for x in visita:
+                Visitas_json.append({
+                    "idVisita":x.Visita.idVisita,
+                    "nombreV":x.Visita.nombre,
+                    "matricula":x.Visita.matriculaVehiculo,
+                    "modelo":x.Visita.modelo,
+                    "color": x.Visita.color,
+                    "estatusV":x.Visita.estatus,
+                    "fechaEntrada":x.Visita.fechaEntrada,
+                    "fechaSalida":x.Visita.fechaSalida,
+                    "idColono":x.Colono.idColono,
+                    "nombre": x.Persona.nombre,
+                    "Apellidos":x.Persona.Apellidos,
+                    "idDomicilio":x.Domicilio.idDomicilio,
+                    "calle": x.Domicilio.calle,
+                    "numero": x.Domicilio.numero,
+                    "descripcion":x.Domicilio.descripcion
+                })
+            return jsonify({"visitas":Visitas_json})
         
     except Exception as e:
         estado = "ERROR"
--- project/Controller/visitaController.py
+++ project/Controller/visitaController.py
@@ -1,39 +1,48 @@
-from ..models import Visita
+from ..models import Visita, Colono, Domicilio,Persona
 from ..__init__ import db
 from datetime import datetime
 
+
 def consultarVisitas(idVisita):
     if idVisita == 0:
-        return db.session.query(Visita).all()
+        return db.session.query(Visita, Colono, Domicilio).join(Colono, Colono.idColono == Visita.idColono).join(Domicilio, Domicilio.idDomicilio == Colono.idDomicilio).join(Persona, Persona.idPersona== Colono.idPersona).all()
     else:
-        return db.session.query(Visita).filter(Visita.idVisita == idVisita).first()
-    
-def agregarVisitas(nombre,matricula,modelo,color,idColono):
-    
+        return db.session.query(Visita, Colono, Domicilio).join(Colono, Colono.idColono == Visita.idColono).join(Domicilio, Domicilio.idDomicilio == Colono.idDomicilio).join(Persona, Persona.idPersona== Colono.idPersona).filter(Visita.idVisita == idVisita).first()
+
+
+def consultarVisitasEmpleado(idColono):
+    if idColono == 0:
+        return db.session.query(Visita, Colono, Domicilio,Persona).join(Colono, Colono.idColono == Visita.idColono).join(Domicilio, Domicilio.idDomicilio == Colono.idDomicilio).join(Persona, Persona.idPersona== Colono.idPersona).all()
+    else:
+        return db.session.query(Visita, Colono, Domicilio).join(Colono, Colono.idColono == Visita.idColono).join(Domicilio, Domicilio.idDomicilio == Colono.idDomicilio).join(Persona, Persona.idPersona== Colono.idPersona).filter(Colono.idColono == idColono).firts()
+
+def agregarVisitas(nombre, matricula, modelo, color, idColono):
+
     visita = Visita(
         nombre=nombre,
         matriculaVehiculo=matricula,
         modelo=modelo,
         color=color,
-        estatus = 1,
-        fechaEntrada = "2000-01-01 00:00:00",
-        fechaSalida = "2000-01-01 00:00:00",
+        estatus=1,
+        fechaEntrada="2000-01-01 00:00:00",
+        fechaSalida="2000-01-01 00:00:00",
         idColono=idColono
     )
-    
+
     db.session.add(visita)
     db.session.commit()
-    
+
     return True
 
 
-def modificarVisita(idVisita,nombre,matricula,modelo,color):
-    visita = db.session.query(Visita).filter(Visita.idVisita == idVisita).first()
+def modificarVisita(idVisita, nombre, matricula, modelo, color):
+    visita = db.session.query(Visita).filter(
+        Visita.idVisita == idVisita).first()
     visita.nombre = nombre
     visita.matriculaVehiculo = matricula
     visita.modelo = modelo
     visita.color = color
-    
+
     db.session.add(visita)
     db.session.commit()
     return True
@@ -41,30 +50,34 @@ def modificarVisita(idVisita,nombre,matricula,modelo,color):
 
 def entradaVisita(idVisita):
     entrada = datetime.now()
-    visita = db.session.query(Visita).filter(Visita.idVisita == idVisita).first()
+    visita = db.session.query(Visita).filter(
+        Visita.idVisita == idVisita).first()
     visita.fechaEntrada = entrada
-    
+
     db.session.add(visita)
     db.session.commit()
-    
+
     return True
+
 
 def salidaVisita(idVisita):
     salida = datetime.now()
-    visita = db.session.query(Visita).filter(Visita.idVisita == idVisita).first()
+    visita = db.session.query(Visita).filter(
+        Visita.idVisita == idVisita).first()
     visita.fechaSalida = salida
-    
+
     db.session.add(visita)
     db.session.commit()
-    
+
     return True
+
 
 def cancelarVisita(idVisita):
-    visita = db.session.query(Visita).filter(Visita.idVisita == idVisita).first()
-    visita.estatus = 0 
-    
+    visita = db.session.query(Visita).filter(
+        Visita.idVisita == idVisita).first()
+    visita.estatus = 0
+
     db.session.add(visita)
     db.session.commit()
-    
-    return True
 
+    return True
